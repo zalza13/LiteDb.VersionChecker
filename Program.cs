4ab7f379052dfbfe309d7b2dfa678ef9ef334cb9
Luciano Pucciarelli <lpucciarelli@qubeyond.com>
2025-04-09 11:22:46 -0600

    C# program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter path to LiteDB file:");
+        var path = Console.ReadLine();
+
+        try
+        {
+            string version = LiteDbVersionDetector.DetectLiteDbVersion(path);
+            Console.WriteLine($"Detected version: {version}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
